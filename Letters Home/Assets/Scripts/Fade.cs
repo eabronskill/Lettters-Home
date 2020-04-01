0a89cd076a7bccbfb87918911f067d3f2d14939c
Kindra Lingenfelter <scrunnchy@gmail.com>
2020-03-31 21:15:17 -0600

    added fade to black

---
+++ Letters Home/Assets/Scripts/Fade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    public GameObject panel;
+    Color transp = new Color(0, 0, 0, 0);
+    Color opaque = new Color(0, 0, 0, 1);
+
+    public void fadeTo(float time)
+    {
+        panel.SetActive(true);
+        BlackSplash(time, true);
+    }
+
+    public void fadeFrom(float time)
+    {
+        BlackSplash(time, false);
+        panel.SetActive(false);
+    }
+
+    void BlackSplash(float time, bool fadeIn)
+    {
+        if (fadeIn)
+        {
+            LeanTween.value(gameObject, transp, opaque, time).setOnUpdate((Color val) =>
+            {
+                panel.GetComponent<Image>().color = val;
+            });
+        }
+        else
+        {
+            LeanTween.value(gameObject, opaque, transp, time).setOnUpdate((Color val) =>
+            {
+                panel.GetComponent<Image>().color = val;
+            });
+        }
+    }
+}
