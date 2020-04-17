0e4464f8b810f67e1bdd1823ab72962910c3332b
Noah Harren <noah.darian@gmail.com>
2020-04-17 16:56:59 -0600

    ragdood works.

--- Letters Home/Assets/ShotTrigger.cs
+++ Letters Home/Assets/ShotTrigger.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class ShotTrigger : MonoBehaviour
 {
-    public DropArm link;
+    public List<DropArm> link = new List<DropArm>();
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            link.trigger();
+            foreach (DropArm arm in link)
+            {
+                arm.trigger();
+            }
             Destroy(this.gameObject);
         }
     }
